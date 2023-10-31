f3c7d5e5deb9abaa2db1f8673b383db3ad66e24a
Selorm <ranselorm@outlook.com>
2023-10-31 03:55:39 +0000

    First commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-primary">
+      <h3>This is the title</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam
+        iure quidem amet quos, corporis ea error reprehenderit at id eius quasi
+        autem beatae soluta nam consequuntur architecto aliquam molestias
+        voluptatem!
+      </p>
+    </div>
+  );
+}
