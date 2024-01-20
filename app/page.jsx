fdb595d74122601bd5e55acc7a24aaaf916ddeca
Selorm <ranselorm@outlook.com>
2024-01-20 19:21:51 +0000

    Refactor

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
-import ChooseMe from "@/components/ChooseMe";
 import Portfolio from "@/components/Portfolio";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
